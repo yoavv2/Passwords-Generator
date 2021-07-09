5f656931b28f1bfd8ab664b2b34ee19bfc2438fb
yoav <23hutc8@gmail.com>
2021-07-09 14:05:38 +0300

    Changed color description, Changed version number

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const log = console.log;
 const createPassword = require("./utils/createPassword");
 const savePassword = require("./utils/savePassword");
 
-program.version("1.0.0").description("Simple Password Generator");
+program
+  .version("1.0.1")
+  .description(chalk.bold.hsl(298, 70, 36)("Simple Password Generator"));
 
 program.option("-l, --length <number>", "length of password", "8");
 program.option("-s, --save", "save passwords to passwords.txt");
